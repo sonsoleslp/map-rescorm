1d7c73651e28d08af7926ef725970fd4c752ab93
Sonsoles López Pernas <sonsoleslp@hotmail.com>
2018-06-18 18:16:46 +0200

    RRD

--- app/components/Map/Mapa.jsx
+++ app/components/Map/Mapa.jsx
@@ -2,15 +2,23 @@ import React from 'react';
 import Map from './Map';
 import {spain} from '../../config/map';
 import '../../assets/scss/map.scss';
-
+import ReactResizeDetector from 'react-resize-detector';
 export default class Mapa extends React.Component {
   constructor(props){
     super(props);
     this.handleClick = this.handleClick.bind(this);
+    this.aspectRatio = this.aspectRatio.bind(this);
+    this.state = {
+      width: 100,
+      height: 100,
+    }
   }
   render(){
     return <div id="map">
-      <Map data={spain} width={'100%'} height={'calc(100% - 200px)'} handleClick={this.handleClick}/>
+      <Map data={spain} width={this.state.width} height={this.state.height} handleClick={this.handleClick}/>
+      <ReactResizeDetector handleWidth handleHeight onResize={(w,h)=>{
+        this.aspectRatio(w,h);
+      }} />
     </div>;
   }
 
@@ -20,6 +28,9 @@ export default class Mapa extends React.Component {
 
   componentDidMount(){
 
+  }
 
+  aspectRatio(width, height) {
+    this.setState({ width, height });
   }
 }
